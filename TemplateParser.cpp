50c7eb96cccd8ad3277037cb95b75567892ab2bd
brennanafrank <66388912+brennanafrank@users.noreply.github.com>
2024-02-18 22:32:30 -0500

    created parser structure

---
+++ TemplateParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Brennan Frank on 2/18/24.
+//
+
+#include "TemplateParser.h"
---
+++ TemplateParser.h
@@ -0,0 +1,16 @@
+//
+// Created by Brennan Frank on 2/18/24.
+//
+
+#ifndef EASYDRAFT_TEMPLATEPARSER_H
+#define EASYDRAFT_TEMPLATEPARSER_H
+
+
+
+class TemplateParser {
+
+};
+
+
+
+#endif //EASYDRAFT_TEMPLATEPARSER_H
