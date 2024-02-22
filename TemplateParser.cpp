bf45775dd2ce04510c092bff581159fb2056ce73
brennanafrank <66388912+brennanafrank@users.noreply.github.com>
2024-02-22 02:49:39 -0500

    added miniz header-only library, more work on zip and unzip

--- TemplateParser.cpp
+++ TemplateParser.cpp
@@ -2,4 +2,51 @@
 // Created by Brennan Frank on 2/18/24.
 //
 
+#include <filesystem>
+#include "miniz-cpp/zip_file.hpp" // miniz tutorial: https://www.informit.com/articles/article.aspx?p=3150354&seqNum=13
 #include "TemplateParser.h"
+
+using namespace std;
+namespace fs = filesystem;
+
+void TemplateParser::setReplacements(const vector<string>& replacements) {
+    this->replacements = replacements;
+}
+
+// the file path has to be in full
+void parse(string fullFilePath) {
+    fs::path tempdocxparsing("./tempdocxparsing");
+    fs::create_directory(tempdocxparsing);
+
+    // rb is mode read-binary, open a binary file for reading
+    FILE* inputDocx = fopen(fullFilePath.c_str(), "rb");
+    if (!inputDocx) {
+        cerr << "There was an error opening the docx input file" << "\n";
+        return;
+    }
+
+    // TODO fix later
+    string outputpath = "";
+    // wb, write a binary file
+    FILE* output = fopen(outputpath.c_str(), "wb");
+    if (!output) {
+        cerr << "There was an error opening the docx output file" << "\n";
+        // close original file descriptor
+        fclose(inputDocx);
+        return;
+    }
+
+    mz_zip_archive zip_archive;
+
+
+
+
+    for (const auto& dirEntry : recursive_directory_iterator("./tempdocxparsing")) {
+        cout << dirEntry << endl;
+    }
+
+    // remove the temporary directory we created
+    system("rm -rf ./tempdocxparsing");
+}
+
+
--- TemplateParser.h
+++ TemplateParser.h
@@ -1,16 +1,28 @@
-//
-// Created by Brennan Frank on 2/18/24.
-//
-
 #ifndef EASYDRAFT_TEMPLATEPARSER_H
 #define EASYDRAFT_TEMPLATEPARSER_H
 
+#include <cstdio>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using recursive_directory_iterator = filesystem::recursive_directory_iterator;
 
 
 class TemplateParser {
+    // path to the docx file
+private:
+    string docxPath = nullptr;
+    vector<string> replacements;
+
+
+    // set the replacements, like from the input module, etc
+
+    void setReplacements(const vector<string>& replacements);
+
 
 };
-
 
 
 #endif //EASYDRAFT_TEMPLATEPARSER_H
