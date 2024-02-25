f22ab82e8359659bc9c6fc09cd1d27cef0bacd17
apendyala9 <apendya@purdue.edu>
2024-02-25 10:39:39 -0500

    Started more work on the widgets, will start the main functionality in the design doc

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,43 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "templates.h"
 
+#include <QDir>
+#include <QFileDialog>
 #include <QPixmap>
+#include <QStandardPaths>
+
+// Q entry list
+// Q list widget
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+/*
+    QWidget *firstPageWidget = new QWidget;
+    QWidget *secondPageWidget = new QWidget;
+    QWidget *thirdPageWidget = new QWidget;
+
+    QStackedWidget *stackedWidget = new QStackedWidget;
+    stackedWidget->addWidget(firstPageWidget);
+    stackedWidget->addWidget(secondPageWidget);
+    stackedWidget->addWidget(thirdPageWidget);
+
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(stackedWidget);
+    setLayout(layout);
+
+    QComboBox *pageComboBox = new QComboBox;
+    pageComboBox->addItem(tr("Page 1"));
+    pageComboBox->addItem(tr("Page 2"));
+    pageComboBox->addItem(tr("Page 3"));
+    connect(pageComboBox, QOverload<int>::of(&QComboBox::activated),
+            stackedWidget, &QStackedWidget::setCurrentIndex);
+*/
 
 }
 
@@ -19,15 +48,30 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    //delete ui->pushButton;
-    //QPixmap pix("/Users/aneeshpendyala/Desktop/IMG_4864.jpeg");
-    //ui->labelWelcome->setPixmap(pix);
-    this->hide();
+    ui->stackedWidget->setCurrentIndex(0);
 
-    Templates temp;
-    temp.setModal(true);
-    temp.exec();
+}
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+    // Get the home directory of the user
+    QString path = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+
+    // Construct a file path to the directory using QDir
+
+    QDir userDir(path);
+
+    // Error checking if the path to the directory exists
+
+    if (!userDir.exists()) {
+
+        qWarning() << "Path not found" << path;
+
+    }
+
+    QString file = QFileDialog::getOpenFileName(nullptr, "Open a File", path);
 
 }
 
