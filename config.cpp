47268203bf8eb1ca3b75d2d1bd0a348c7f276c57
kcj4104 <86736977+kcj4104@users.noreply.github.com>
2024-02-25 14:32:12 -0500

    Commiting work for scrum meeting

--- config.cpp
+++ config.cpp
@@ -1,5 +1,8 @@
 #include "config.hpp"
 
+// Function to read the configuration file and
+// fill out the variables that have values in
+// the config file. This would be used on startup.
 int read_config() {
     // Check if file exists
     if (!fs::exists("config.json")) {
@@ -23,20 +26,42 @@ int read_config() {
     config_file.close();
 
     // Fill out variables with config info here
+    for (json::iterator it = config.begin(); it != config.end(); ++it) {
 
-}
-
-// Function to create config.json and input all
-// information needed for the program to run.
-int create_fill_config() {
-    std::ofstream config_file("config.json", std::ios::out);
-    if (!config_file.is_open()) {
-        return -1;
     }
 
     return 0;
 }
 
+// Function to create config.json and input all
+// information needed for the program to run.
+int create_fill_config() {
+    //std::ofstream config_file("config.json", std::ios::out);
+    std::ofstream config_file("config.json");
+    if (!config_file.is_open()) {
+        return -1;
+    }
+
+    // Input defualt info
+    json default_config = {
+        {"paths", {
+            {"current_dir", nullptr},
+            {"import_dir", nullptr},
+            {"export_dir", nullptr}
+        }},
+        {"misc", {
+            {"converter", nullptr}
+        }}
+    };
+
+    config_file << default_config.dump(4) << std::endl;
+    config_file.close();
+
+    return 0;
+}
+
+// Function to change the value of a configuration
+// in the file.
 int change_config() {
     return 0;
 }
