4020a65bc06538a62b561f1d20d117affd0b15cd
kcj4104 <86736977+kcj4104@users.noreply.github.com>
2024-02-29 18:28:20 -0500

    Finished config.cpp & .hpp

--- file_operations.hpp
+++ file_operations.hpp
@@ -15,6 +15,7 @@ int upload_template(fs::path filepath);
 int delete_template(std::string filename);
 int save_document(std::string filename);
 std::vector<std::string> template_list();
+std::vector<std::string> search_template_list();
 int docx_convert(std::string filename, std::string file_ext);
 int libre_converter(std::string filename, std::string file_ext);
 int pandoc_converter(std::string filename, std::string file_ext);
