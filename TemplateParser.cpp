3417ce5aefd78c481d43ee9639a7d3b030d5197f
kcj4104 <86736977+kcj4104@users.noreply.github.com>
2024-02-29 16:07:17 -0500

    Fixed upload_file()

--- TemplateParser.cpp
+++ TemplateParser.cpp
@@ -18,8 +18,64 @@ using recursive_directory_iterator = filesystem::recursive_directory_iterator;
 
 class TemplateParser {
 public:
-    TemplateParser();
-    void parse(const string& fullFilePath);
+    void setReplacements(const vector<string>& replacements) {
+        this->replacements = replacements;
+    }
+
+    TemplateParser() {}
+
+    // the file path to our docx/zip file has to be in full
+    void parse(const string& fullFilePath) {
+        cout << "here1 \n";
+        fs::path tempdocxparsing("./tempdocxparsing");
+        cout << "here1 \n";
+        fs::create_directory(tempdocxparsing);
+        cout << "here1 \n";
+
+        const fs::path docxpath("stat511.docx");
+
+        /*
+        // rb is mode read-binary, open a binary file for reading
+        FILE* inputDocx = fopen(fullFilePath.c_str(), "rb");
+        if (!inputDocx) {
+            cerr << "There was an error opening the docx input file" << "\n";
+            return;
+        }
+
+        // TODO fix later
+        string outputpath = "";
+        // wb, write a binary file
+        FILE* output = fopen(outputpath.c_str(), "wb");
+        if (!output) {
+            cerr << "There was an error opening the docx output file" << "\n";
+            // close original file descriptor
+            fclose(inputDocx);
+            return;
+        }
+        */
+
+        ZipArchive input; // load zipFileName
+        input.create(docxpath);
+        cout << "here2\n";
+        for (string s : input.entryNames()) {
+            cout << s << "\n";
+        }
+        cout << "here3\n";
+
+        mz_zip_archive zip_archive;
+
+
+
+
+        /* later */
+        for (const auto& dirEntry : recursive_directory_iterator("./tempdocxparsing")) {
+            cout << dirEntry << endl;
+        }
+
+        // remove the temporary directory we created
+        system("rm -rf ./tempdocxparsing"); // #TODO make system independent
+    }
+
 private:
     // path to the docx file
     string docxPath = "";
@@ -28,70 +84,10 @@ private:
 
     // set the replacements, like from the input module, etc
 
-    void setReplacements(const vector<string>& replacements);
+    //void setReplacements(const vector<string>& replacements);
 
 
 };
-
-
-
-void TemplateParser::setReplacements(const vector<string>& replacements) {
-    this->replacements = replacements;
-}
-
-TemplateParser::TemplateParser() {}
-
-// the file path to our docx/zip file has to be in full
-void TemplateParser::parse(const string& fullFilePath) {
-    cout << "here1 \n";
-    fs::path tempdocxparsing("./tempdocxparsing");
-    cout << "here1 \n";
-    fs::create_directory(tempdocxparsing);
-    cout << "here1 \n";
-
-    const fs::path docxpath("stat511.docx");
-
-    /*
-    // rb is mode read-binary, open a binary file for reading
-    FILE* inputDocx = fopen(fullFilePath.c_str(), "rb");
-    if (!inputDocx) {
-        cerr << "There was an error opening the docx input file" << "\n";
-        return;
-    }
-
-    // TODO fix later
-    string outputpath = "";
-    // wb, write a binary file
-    FILE* output = fopen(outputpath.c_str(), "wb");
-    if (!output) {
-        cerr << "There was an error opening the docx output file" << "\n";
-        // close original file descriptor
-        fclose(inputDocx);
-        return;
-    }
-     */
-
-    KZip::ZipArchive input; // load zipFileName
-    input.create(docxpath);
-    cout << "here2\n";
-    for (string s : input.entryNames()) {
-        cout << s << "\n";
-    }
-    cout << "here3\n";
-
-    mz_zip_archive zip_archive;
-
-
-
-
-    /* later */
-    for (const auto& dirEntry : recursive_directory_iterator("./tempdocxparsing")) {
-        cout << dirEntry << endl;
-    }
-
-    // remove the temporary directory we created
-    system("rm -rf ./tempdocxparsing"); // #TODO make system independent
-}
 
 
 
--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #ifndef MAIN_CPP
 #define MAIN_CPP
 
+
+//#include "TemplateParser.cpp"
 int main() {
     //TemplateParser tp{};
-    //tp.parse("stat511.docx");
+    tp.parse("stat511.docx");
     return 0;
 }
 #endif
