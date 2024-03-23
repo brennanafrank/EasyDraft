9b3f1e422ca77c559f7d849b33bf936d9ee9d91b
sergin17 <alvarezsergio1794@gmail.com>
2024-03-23 09:38:57 -0400

    Minor change to recycle_bin.cpp

--- recycle_bin.cpp
+++ recycle_bin.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cstdio>
 
+
 void move_to_trash(char* filename) {
     // Function to move a file to the trash directory
 
-    std::string trash_path = CURRENT_DIR.string() + "/trash/" + filename;
+    std::string trash_path =  "./trash/" + std::string(filename);
 
     if (std::rename(filename, trash_path.c_str()) != 0) {
         std::cout << "Error moving file to trash" << std::endl;
@@ -14,5 +15,4 @@ void move_to_trash(char* filename) {
 
     return;
 }
-
 
