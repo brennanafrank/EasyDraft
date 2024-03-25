fb0fed7464d45cf950953b0554e863263fe5651f
Michael8021 <zyangbz@ust.hk>
2024-03-25 19:04:28 -0400

    use nlohmann json library

--- parsing.cpp
+++ parsing.cpp
@@ -1,33 +1,34 @@
-#include <iostream>
+// DocumentModifier.cpp
+#include "parsing.h"
 #include <fstream>
-#include <string>
-#include <cstdlib> 
-#include <vector>
-#include <sstream>
+#include <iostream>
+#include <cstdlib>
 
 std::string vectorToJson(const std::vector<std::pair<std::string, std::vector<std::string>>>& replacements) {
-    std::stringstream json;
-    json << "{";
-    for (auto it = replacements.begin(); it != replacements.end(); ++it) {
-        json << "\"" << it->first << "\": [";
-        for (auto vecIt = it->second.begin(); vecIt != it->second.end(); ++vecIt) {
-            json << "\"" << *vecIt << "\"";
-            if (vecIt != it->second.end() - 1) json << ", ";
-        }
-        json << "]";
-        if (it != replacements.end() - 1) json << ", ";
+    nlohmann::json j;
+    for (const auto& p : replacements) {
+        j[p.first] = p.second;
     }
-    json << "}";
-    return json.str();
+    return j.dump();
 }
 
+std::vector<std::pair<std::string, std::vector<std::string>>> jsonToVector(const std::string& jsonStr) {
+    nlohmann::json j = nlohmann::json::parse(jsonStr);
+    std::vector<std::pair<std::string, std::vector<std::string>>> replacements;
+
+    for (auto& el : j.items()) {
+        replacements.push_back({el.key(), el.value().get<std::vector<std::string>>()});
+    }
+
+    return replacements;
+}
 
 void callPythonScript(const std::string& docPath, const std::string& replacementsPath) {
     std::string command = "python modify_docx.py " + docPath + " " + replacementsPath;
     system(command.c_str());
 }
 
-void modifyDocument(const std::string& docPath, const std::string& replacementsJson, const std::string& tempJsonPath = "./replacements.json") {
+void modifyDocument(const std::string& docPath, const std::string& replacementsJson, const std::string& tempJsonPath) {
     std::ofstream outFile(tempJsonPath);
     if (!outFile) {
         std::cerr << "Error opening file for writing: " << tempJsonPath << std::endl;
@@ -36,23 +37,7 @@ void modifyDocument(const std::string& docPath, const std::string& replacementsJ
     outFile << replacementsJson;
     outFile.close();
 
-
     callPythonScript(docPath, tempJsonPath);
 
     std::remove(tempJsonPath.c_str());
-}
-
-int main() {
-    std::vector<std::pair<std::string, std::vector<std::string>>> replacements = {
-        {"Name", {"Michael", "Sarah", "Alex",}},
-        {"Address", {"NY", "LA", "HK"}},
-        {"Phone", {"123456", "654321", "987654"}}
-    };
-
-    // Path to the document to be modified
-    std::string docPath = "./test_doc.docx";
-
-    modifyDocument(docPath, vectorToJson(replacements));
-
-    return 0;
 }
