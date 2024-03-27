98f3d0dcd276eabc3cf03450c731e66a7206443d
sergin17 <alvarezsergio1794@gmail.com>
2024-03-27 18:25:07 -0400

    minor changes

--- user_profile.cpp
+++ user_profile.cpp
@@ -3,50 +3,55 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-void createProfile(std::vector<std::vector<std::string>> placeholders) {
+//convert to user json files, to immplement with Micheal's json files.
+
+
+typedef std::vector<std::vector<std::string>> ValueVector;
+
+void createProfile(ValueVector placeholders, std::string fileName) {
     //create a new profile
-    std::string fileName;
-    std::cout << "Enter a new profile name: ";
-    std::cin >> fileName;
-
     //check if file exists
-        if (FILE *file = fopen(fileName.c_str(), "r")) {
-            std::cout << "User profile already exists. Do you want to add new placeholders? (y/n): ";
-            char choice;
-            std::cin >> choice;
-            if (choice == 'n' || choice == 'N') {
-                fclose(file);
-                return;
-            }
+    if (FILE *file = fopen(fileName.c_str(), "r")) {
+        std::cout << "User profile already exists. Do you want to add new placeholders? (y/n): "; //change to qt message box
+        char choice;
+        std::cin >> choice;
+        if (choice == 'n' || choice == 'N') {
             fclose(file);
+            return;
         }
-        else {
-            
-            std::ofstream outfile;
-            outfile.open(fileName, std::ios_base::app);
-            for (int i = 0; i < placeholders.size(); i++) {
-                outfile << placeholders[i][0] << std::endl;
-                outfile << placeholders[i][1] << std::endl;
-            }
-            
-            fclose(file);
+        fclose(file);
+    }
+    else {
+
+        std::ofstream outfile;
+        outfile.open(fileName, std::ios_base::app);
+        for (int i = 0; i < placeholders.size(); i++) {
+            outfile << placeholders[i][0] << std::endl;
+            outfile << placeholders[i][1] << std::endl;
         }
+
+        fclose(file);
+    }
 }
-void updatePlaceholders(std::string fileName, std::string placeholder, std::string value) {
+void updatePlaceholder(std::string fileName, std::string placeholder, std::string value) {
     std::ifstream file(fileName);
     std::ofstream tempFile("temp.txt");
     std::string line;
-
+    int reached = 0;
     while (std::getline(file, line)) {
         if (line == placeholder) {
             tempFile << line << std::endl;
             tempFile << value << std::endl;
+            reached = 1;
         } else {
             tempFile << line << std::endl;
         }
 
+    }
+    if (reached == 0) {
+        std::cout << "Placeholder not found" << std::endl; //change to qt message box
+        return;
     }
 
     file.close();
@@ -54,7 +59,7 @@ void updatePlaceholders(std::string fileName, std::string placeholder, std::stri
 
     std::remove(fileName.c_str());
     std::rename("temp.txt", fileName.c_str());
-    
+
 }
 
 void deleteProfile(std::string fileName) {
@@ -68,67 +73,27 @@ void addProfilePlaceholder(std::string fileName, std::string placeholder, std::s
     outfile << value << std::endl;
 }
 
-void fillPlaceholders(std::string fileName, std::vector<std::vector<std::string>> placeholders ) {
-    std::unordered_map<std::string, std::string> profilePlaceholders = getProfile(fileName);
-    std::string choice;
-
-    std::cout << "Do you want to autofill(1) or select placeholders to match(2)? (1/2): ";
-    std::cin >> choice;
-    if (choice == "1") {
-        //autofill
-        for (const auto& pair : profilePlaceholders) {
-            //check each placeholder in profile if it matches one placeholder in placeholders array
-            if (std::find(placeholders[0].begin(), placeholders[0].end(), pair.first) != placeholders.end()) {
-                //find text box related to the placeholder
-
-                //fill in the text box with the value in profile (pair.second)
-                
-            }
-            else {
-                //if it doesn't match, ask if user wants to add it to another text box
-
-                //if yes 
-            }
-        }
-        
-    }
-    else if (choice == "2") {
-        //select placeholders
-    }
-    else {
-        std::cout << "Invalid choice. Please enter 1 or 2." << std::endl;
-    }
-
-}
-
-
-std::unordered_map<std::string, std::string> getProfile(const std::string& fileName) {
-    std::unordered_map<std::string, std::string> dictionary;
+ValueVector getProfile(std::string fileName) {
     std::ifstream file(fileName);
     std::string line;
-    int lineNumber = 1;
-
+    ValueVector placeholders;
+    int i = 0;
     while (std::getline(file, line)) {
-        std::string key = line;
-        std::getline(file, line);
-        std::string value = line;
-        dictionary[key] = value;
-        
-        lineNumber++;
+        placeholders[i][0] = line;
+        if (std::getline(file, line))
+            placeholders[i][1];
+        i++;
     }
-
-    return dictionary;
+    return placeholders;
 }
 
-int main() {
-    std::string fileName;
-    std::cout << "Enter a file name: ";
-    std::cin >> fileName;
-    fillPlaceholders(fileName);
-   
-
+//maybe not needed
+void fillPlaceholders(std::string fileName, std::vector<std::vector<std::string>> placeholders ) {
+    ValueVector profilePlaceholders = getProfile(fileName);
+    std::string choice;
     
 
-    
-    return 0;
 }
+
+
+
