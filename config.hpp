dbcc5e1a4870d480ebcea32ee3caa69553134641
Michael8021 <zyangbz@ust.hk>
2024-04-10 23:09:41 -0400

    fix config to header only

--- config.hpp
+++ config.hpp
@@ -1,16 +1,14 @@
-/*
- * Kye Jocham
-*/
-
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
 
 #include <fstream>
 #include <iostream>
 #include <filesystem>
 #include <ctime>
-#include <QString>
 #include "json.hpp"
+#include <QString>
+#include <vector>
+#include <chrono>
 
 using json = nlohmann::json;
 namespace fs = std::filesystem;
@@ -18,40 +16,54 @@ namespace fs = std::filesystem;
 // Define a custom type for a vector of integers
 typedef std::vector<std::pair<std::string, std::vector<std::string>>> PlaceholderPair;
 
+// Inline global variables for paths
+inline fs::path CURRENT_DIR = (fs::current_path()).string();
+inline fs::path IMPORT_DIR = (fs::current_path().append("templates")).string();
+inline fs::path EXPORT_DIR = "/Users/aneeshpendyala/Desktop/ExportTemplates";
+inline fs::path DRAFTS_DIR = (fs::current_path().append("drafts")).string();
+inline fs::path LOG_DIR = (fs::current_path().append("logs")).string();
+inline const std::string PYTHON_EXEC_PATH = "/Users/michael/anaconda3/bin/python";
+inline const std::string PROJECT_PATH = "/Users/michael/Developer/EasyDraft";
+inline const QString TAG_MANAGER_JSON_PATH = QString::fromStdString((fs::current_path() / "tag.json").string());
+inline const QString TEMPLATES_PATH = "/Users/michael/Desktop";
 
-// Defining global variables for config.hpp.
-// Use if using read_config and config.json
-/*
-// "paths"
-fs::path CURRENT_DIR;
-fs::path IMPORT_DIR;
-fs::path EXPORT_DIR;
+// Inline global variables for misc
+inline std::string CONVERTER = "None";
+inline std::chrono::hours LOG_DELETE_TIME(24); // One day
 
-// "misc"
-std::string CONVERTER;
-*/
 
-// Use if not using read_config and config.json.
-// This is used to make testing easier. 
-// Will still need to compile config.cpp for program to run.
-fs::path CURRENT_DIR = (fs::current_path()).string();
-fs::path IMPORT_DIR = (fs::current_path().append("templates")).string();
-//fs::path EXPORT_DIR = (fs::current_path().append("output")).string();
-fs::path EXPORT_DIR = ("/Users/aneeshpendyala/Desktop/ExportTemplates");
-fs::path DRAFTS_DIR = (fs::current_path().append("drafts")).string();
-fs::path LOG_DIR = (fs::current_path().append("logs")).string();
-std::string CONVERTER = "None";
-std::chrono::hours LOG_DELETE_TIME(24); // One day
-const std::string PYTHON_EXEC_PATH = "/Library/Frameworks/Python.framework/Versions/3.12/bin/python3";
-const std::string PROJECT_PATH = "/Users/aneeshpendyala/Desktop/EasyDraft";
-const QString TAG_MANAGER_JSON_PATH = "/Users/aneeshpendyala/Desktop/Json Directory/config.json";
-const QString TEMPLATES_PATH = "/Users/aneeshpendyala/Desktop";
+// Function to create config.json and input all
+// information needed for the program to run.
+inline int create_fill_config() {
+    std::ofstream config_file("config.json");
+    if (!config_file.is_open()) {
+        return -1;
+    }
+
+    // Input default info
+    json default_config = {
+        {"paths", {
+                      {"CURRENT_DIR", (fs::current_path()).string()},
+                      {"IMPORT_DIR", (fs::current_path().append("templates")).string()},
+                      {"EXPORT_DIR", (fs::current_path().append("output")).string()},
+                      {"DRAFTS_DIR", (fs::current_path().append("drafts")).string()}
+                  }},
+        {"misc", {
+                     {"CONVERTER", "None"}
+                 }}
+    };
+
+    config_file << default_config.dump(4) << std::endl;
+    config_file.close();
+
+    return 0;
+}
 
 
 // Function to read the configuration file and
 // fill out the variables that have values in
 // the config file. This would be used on startup.
-int read_config() {
+inline int read_config() {
     // Check if file exists
     if (!fs::exists(fs::current_path().append("config.json"))) {
         if (create_fill_config() != 0) {
@@ -86,52 +98,25 @@ int read_config() {
     return 0;
 }
 
-// Function to create config.json and input all
-// information needed for the program to run.
-int create_fill_config() {
-    std::ofstream config_file("config.json");
-    if (!config_file.is_open()) {
-        return -1;
-    }
-
-    // Input defualt info
-    json default_config = {
-        {"paths", {
-            {"CURRENT_DIR", (fs::current_path()).string()},
-            {"IMPORT_DIR", (fs::current_path().append("templates")).string()},
-            {"EXPORT_DIR", (fs::current_path().append("output")).string()},
-            {"DRAFTS_DIR", (fs::current_path().append("drafts")).string()}
-        }},
-        {"misc", {
-            {"CONVERTER", "None"}
-        }}
-    };
-
-    config_file << default_config.dump(4) << std::endl;
-    config_file.close();
-
-    return 0;
-}
 
 // Function to change the value of a configuration
 // in the file.
-int change_config(std::string key, std::string value, bool json_refresh) {
-    if (key == "CURRENT_DIR") {    
+inline int change_config(std::string key, std::string value, bool json_refresh) {
+    if (key == "CURRENT_DIR") {
         CURRENT_DIR = value;
     } else if (key == "IMPORT_DIR") {
         IMPORT_DIR = value;
     } else if (key == "EXPORT_DIR") {
         EXPORT_DIR = value;
-    } else if (key == "DRAFT_DIR") {
+    } else if (key == "DRAFTS_DIR") {
         DRAFTS_DIR = value;
     } else if (key == "CONVERTER") {
         CONVERTER = value;
     } else if (key == "n/a") {
-        
+
     } else {
         return -1;
     }
-    return 0;
 
     if (json_refresh) {
         std::string config_filename = "config.txt";
@@ -141,14 +126,14 @@ int change_config(std::string key, std::string value, bool json_refresh) {
 
         json config = {
             {"paths", {
-                {"CURRENT_DIR", CURRENT_DIR},
-                {"IMPORT_DIR", IMPORT_DIR},
-                {"EXPORT_DIR", EXPORT_DIR},
-                {"DRAFT_DIR", DRAFTS_DIR}
-            }},
+                          {"CURRENT_DIR", CURRENT_DIR},
+                          {"IMPORT_DIR", IMPORT_DIR},
+                          {"EXPORT_DIR", EXPORT_DIR},
+                          {"DRAFTS_DIR", DRAFTS_DIR}
+                      }},
             {"misc", {
-                {"CONVERTER", CONVERTER}
-            }}
+                         {"CONVERTER", CONVERTER}
+                     }}
         };
 
         std::ofstream config_file("config.json");
@@ -162,8 +147,8 @@ int change_config(std::string key, std::string value, bool json_refresh) {
     return 0;
 }
 
-// Funciton to reset the config file to defaults.
-int reset_config() {
+// Function to reset the config file to defaults.
+inline int reset_config() {
     std::string config_file = "config.txt";
 
     if (fs::exists(config_file)) {
@@ -174,5 +159,7 @@ int reset_config() {
     }
 }
 
+// Avoid using extern unless you really need to link to variables defined elsewhere
+extern int err_count;
 
-#endif /* CONFIG_H */
+#endif /* CONFIG_HPP */
